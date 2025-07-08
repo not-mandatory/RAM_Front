2eeb8e92fd1e0776bedd0372983e2f72504e0ea0
not_mandatory <berrabeh.anass@ensam-casa.ma>
2025-07-08 17:28:40 +0100

    fixed usesearchparams

--- src/app/admin/project/new/page.tsx
+++ src/app/admin/project/new/page.tsx
@@ -1,11 +1,13 @@
 import { ProjectForm } from "@/components/projects/project-form"
+import { Suspense } from "react";
 
 export default function NewProjectPage() {
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-3xl font-bold mb-6">Créer un nouveau projet</h1>
-      <ProjectForm />
-    </div>
+      <Suspense fallback={<div>Loading form…</div>}>
+        <ProjectForm />
+      </Suspense>    </div>
   )
 }
 
